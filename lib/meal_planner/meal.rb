651128ce2beb0ef1e06167e134b6beba5c09a548
Peter Sumsion <sumsionp@gmail.com>
2013-11-22 07:06:36 -0700

    Added meal time to meal to track default time for a specific meal

--- lib/meal_planner/meal.rb
+++ lib/meal_planner/meal.rb
@@ -1,9 +1,12 @@
 class Meal
-  def initialize(title)
-    @title = title
+  attr_reader :main_dish, :typical_time
+
+  def initialize(main_dish, typical_time)
+    @main_dish = main_dish
+    @typical_time = typical_time.capitalize
   end
 
   def to_s
-    "Meal: #{@title}"
+    "Main Dish: #{@main_dish}, Typical Time: #{@typical_time}"
   end
 end
--- spec/meal_planner/meal_spec.rb
+++ spec/meal_planner/meal_spec.rb
@@ -2,16 +2,20 @@ require 'meal_planner/meal'
 
 describe Meal do
   before do
-    @meal = Meal.new("Rice and Meat Sauce")
+    @main_dish = "Rice and Meat Sauce"
+    @typical_time = "Lunch"
+    @meal = Meal.new(@main_dish, @typical_time)
   end
 
-  it "has a main dish"
+  it "has a main dish" do
+    @meal.main_dish.should == @main_dish
+  end
 
-  it "has a list of suggested side dishes"
-
-  it "has a typical meal time"
+  it "has a typical meal time" do
+    @meal.typical_time.should == @typical_time
+  end
 
   it "has a string representation" do
-    @meal.to_s.should == "Meal: Rice and Meat Sauce"
+    @meal.to_s.should == "Main Dish: #{@main_dish}, Typical Time: #{@typical_time}"
   end
 end
