c7d92c7cf29b4c19813aa3a431e100d5a13e9e2e
Peter Sumsion <sumsionp@gmail.com>
2013-11-23 14:42:54 -0700

    Defined initial test stubs for version 1.0.0

--- spec/meal_planner/meal_spec.rb
+++ spec/meal_planner/meal_spec.rb
@@ -3,33 +3,31 @@ require 'meal_planner/meal'
 describe Meal do
   before do
     @main_dish = "Rice and Meat Sauce"
-    @typical_time = "Lunch"
-    @meal = Meal.new(@main_dish, @typical_time)
+    @meal = Meal.new(@main_dish)
   end
 
   it "has a main dish" do
     @meal.main_dish.should == @main_dish
   end
 
-  it "has a typical meal time" do
-    @meal.typical_time.should == @typical_time
-  end
-
   it "has a string representation" do
-    @meal.to_s.should == "Main Dish: #{@main_dish}, Typical Time: #{@typical_time}"
+    @meal.to_s.should == "Main Dish: #{@main_dish}, Sides: "
   end
 
   context "suggested sides have been added" do
     before do
       @main_dish = "Hashed Browns"
-      @typical_time = "Dinner"
-      @meal = Meal.new(@main_dish, @typical_time)
+      @meal = Meal.new(@main_dish)
 
-      @side1 = Recipe.new("Bacon")
-      @meal.add_suggested_side(@side1)
+      @side1 = "Bacon"
+      @side2 = "Salsa"
+      @meal.add_side(@side1)
+      @meal.add_side(@side2)
     end
 
-    it "has a suggested side"
+    it "returns each side"
+
+    it "lists sides in string representation"
 
   end
 end
--- spec/meal_planner/menu_spec.rb
+++ spec/meal_planner/menu_spec.rb
@@ -2,9 +2,13 @@ require 'meal_planner/menu.rb'
 
 describe Menu do
   before do
-
+    @menu = Menu.new(3)
   end
 
-  it "has a list of dates each with appropriate meal times"
+  it "has a list of menu days"
+  # menu_days
+
+  it "switches a meal with one that is not already in the list"
+  # switch(menu_day)
 
 end
