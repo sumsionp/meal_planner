699c123ee2a3019c483907f18caab24cb9212ef8
Peter Sumsion <sumsionp@gmail.com>
2013-11-28 08:37:49 -0700

    Add and pass initial attribute tests for MenuDay

--- lib/meal_planner/menu_day.rb
+++ lib/meal_planner/menu_day.rb
@@ -1,2 +1,12 @@
+require_relative 'meal'
+
 class MenuDay
+  attr_reader :date, :breakfast, :lunch, :dinner
+
+  def initialize(date, breakfast=nil, lunch=nil, dinner=nil)
+    @date = date
+    @breakfast = breakfast
+    @lunch = lunch
+    @dinner = dinner
+  end
 end
--- spec/meal_planner/menu_day_spec.rb
+++ spec/meal_planner/menu_day_spec.rb
@@ -3,8 +3,21 @@ require 'meal_planner/menu_day'
 describe MenuDay do
 
   before do
+    @date = Date.new(2013, 10, 21)
+    @breakfast = Meal.new("Bacon and Eggs")
+    @lunch = Meal.new("Sandwiches")
+    @dinner = Meal.new("Spaghetti")
+
+    @menu_day = MenuDay.new(@date, @breakfast, @lunch, @dinner)
   end
 
-  it "has breakfast, lunch, and dinner"
+  it "has a date" do
+    @menu_day.date.should == @date
+  end
 
+  it "has breakfast, lunch, and dinner" do
+    @menu_day.breakfast.should == @breakfast
+    @menu_day.lunch.should == @lunch
+    @menu_day.dinner.should == @dinner
+  end
 end
