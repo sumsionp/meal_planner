388e80883328286a0844e711d3c506b7119d118c
Peter Sumsion <sumsionp@gmail.com>
2013-12-04 06:58:50 -0700

    Add and pass test for returning each menu day

--- lib/meal_planner/menu.rb
+++ lib/meal_planner/menu.rb
@@ -7,5 +7,16 @@ class Menu
   def initialize(number_of_days)
     @number_of_days = number_of_days
     @start_date = Time.new.to_date
+    @menu_days = []
+
+    0.upto(@number_of_days) do |i|
+      @menu_days << MenuDay.new(@start_date + i)
+    end
+  end
+
+  def each_menu_day
+    if block_given?
+      @menu_days.each { |menu_day| yield menu_day }
+    end
   end
 end
--- spec/meal_planner/menu_spec.rb
+++ spec/meal_planner/menu_spec.rb
@@ -1,11 +1,21 @@
 require 'meal_planner/menu.rb'
 require 'timecop'
+require 'date'
 
 describe Menu do
   before do
+    Timecop.freeze(Date.new(2013, 7, 24))
+    @menu = Menu.new(2)
   end
 
-  it "adds a menu day"
+  it "returns each menu day" do
+    test_dates = [Date.new(2013, 7, 24), Date.new(2013, 7, 25)]
+    expected_dates = []
+
+    @menu.each_menu_day do |menu_day|
+      expected_dates << menu_day.date
+    end
+  end
 
   it "switches a meal with one that is not already in the list"
   # switch(menu_day)
