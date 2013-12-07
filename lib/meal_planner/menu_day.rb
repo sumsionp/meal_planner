e3367e1246850ebea696c4c3e9e8ca1c07934f40
Peter Sumsion <sumsionp@gmail.com>
2013-12-07 16:12:17 -0700

    Remove support for breakfast since that won't be used in the first release

--- lib/meal_planner/menu_day.rb
+++ lib/meal_planner/menu_day.rb
@@ -4,16 +4,14 @@ class MenuDay
   attr_reader :date
   attr_accessor :breakfast, :lunch, :dinner
 
-  def initialize(date, breakfast=nil, lunch=nil, dinner=nil)
+  def initialize(date, lunch=nil, dinner=nil)
     @date = date
-    @breakfast = breakfast
     @lunch = lunch
     @dinner = dinner
   end
 
   def each_meal
     if block_given?
-      yield @breakfast
       yield @lunch
       yield @dinner
     end
--- spec/meal_planner/menu_day_spec.rb
+++ spec/meal_planner/menu_day_spec.rb
@@ -4,34 +4,29 @@ describe MenuDay do
 
   before do
     @date = Date.new(2013, 10, 21)
-    @breakfast = Meal.new("Bacon and Eggs")
     @lunch = Meal.new("Sandwiches")
     @dinner = Meal.new("Spaghetti")
 
-    @breakfast2 = Meal.new("Pancakes")
     @lunch2 = Meal.new("Leftovers")
     @dinner2 = Meal.new("Chicken and Rice")
 
-    @menu_day = MenuDay.new(@date, @breakfast, @lunch, @dinner)
+    @menu_day = MenuDay.new(@date, @lunch, @dinner)
   end
 
   it "has a date" do
     @menu_day.date.should == @date
   end
 
-  it "has breakfast, lunch, and dinner" do
-    @menu_day.breakfast.should == @breakfast
+  it "has lunch, and dinner" do
     @menu_day.lunch.should == @lunch
     @menu_day.dinner.should == @dinner
   end
 
-  it "allows breakfast, lunch, and dinner to be changed" do
-    @menu_day.breakfast = @breakfast2
+  it "allows lunch, and dinner to be changed" do
     @menu_day.lunch = @lunch2
     @menu_day.dinner = @dinner2
 
 
-    @menu_day.breakfast.should == @breakfast2
     @menu_day.lunch.should == @lunch2
     @menu_day.dinner.should == @dinner2
  end
